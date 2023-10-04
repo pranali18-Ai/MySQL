bfb5baf5ef269fcdc0adc0cacb804a4a45e66c2f
Pranali Patil <patilpranali0018@gmail.com>
2023-10-04 12:41:30 +0530

    4/10/2023

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -18,3 +18,17 @@ INSERT INTO Kiarastudent VALUES(2, 'Pranali', 21);
 
 --To view TABLE 
 SELECT * FROM Kiarastudent;
+
+--To create database using IF NOT EXISTS
+IF NOT EXISTS(SELECT NAME FROM sys.databases WHERE name = 'KiaraStudent')
+BEGIN
+CREATE DATABASE kiaraStudent;
+END
+
+--To show the databases
+Select name
+From sys.databases
+where database_id>4;
+
+
+
